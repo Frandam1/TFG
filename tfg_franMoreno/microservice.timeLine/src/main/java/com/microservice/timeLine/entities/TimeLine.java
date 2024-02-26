b8e4b61ebebf32746568464e2c2d4067d008cc12
= <=>
2024-02-26 15:02:31 +0100

    Empezando timeline

---
+++ tfg_franMoreno/microservice.timeLine/src/main/java/com/microservice/timeLine/entities/TimeLine.java
@@ -0,0 +1,20 @@
+package com.microservice.timeLine.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "timeline")
+public class TimeLine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
