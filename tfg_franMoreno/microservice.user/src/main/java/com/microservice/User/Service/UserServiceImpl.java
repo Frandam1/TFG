9e47b38d4d1fdad2e503004d8c91d632de3547d9
= <=>
2024-02-26 14:16:10 +0100

    Create UserServiceImpl.java
    Creando la implementacion del servicio, añadiendo los metodos por defeceto

---
+++ tfg_franMoreno/microservice.user/src/main/java/com/microservice/User/Service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.microservice.User.Service;
+
+import com.microservice.User.entities.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements InterfaceUserService{
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public List<User> findByIdTimeLine(Long idTimeLine) {
+        return null;
+    }
+}
